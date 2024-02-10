a814aaf05e9f7cbe5471c0166bf2cf4609dbc7c7
zaemon <94009680+aryan-0543@users.noreply.github.com>
2024-02-10 21:08:49 +0530

    HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -5,15 +5,15 @@ import math
 import numpy as np
 
 class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=3,complexity=1, detectionCon=0.5, trackCon = 0.5):
         self.mode = mode
         self.maxHands = maxHands
+        self.complexity = complexity
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
-        self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.complexity, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
 
@@ -94,7 +94,7 @@ def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
 def main():
     pTime = 0
     cTime = 0
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     detector = handDetector()
     while True:
         success, img = cap.read()
